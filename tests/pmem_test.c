5ed0ae0a62ca2aefa7808df8400e5d172d840216
nickscha <nick@scharnbeck.de>
2025-08-30 17:44:29 +0200

    [VERSION] bump to v0.2

--- tests/pmem_test.c
+++ tests/pmem_test.c
@@ -1,4 +1,4 @@
-/* pmem.h - v0.1 - public domain data structures - nickscha 2025
+/* pmem.h - v0.2 - public domain data structures - nickscha 2025
 
 A C89 standard compliant, single header, nostdlib (no C Standard Library) Platform Memory Provider (PMEM).
 
